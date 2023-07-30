f0e6f6b76ad62ed087c5d3e622cd3f36243390b1
CulturedGamer <dony357@outlook.com>
2023-07-29 19:37:21 -0700

    update

--- flake.nix
+++ flake.nix
@@ -1,22 +1,13 @@
 {
-    description = "My build of dwm";
+    description = "Custom build of dwm";
+    
+    inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs";
-    };
-
-    outputs = { self, nixpkgs }: 
-    let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-    in {
-        packages.${system} = {
-            dwmPackage = pkgs.dwm.overrideAttrs (_: {
-                version = "main";
-                src = ./.;
-                patches = [ ];
-            });
-            default = self.packages.${system}.dwmPackage;
-        };
+    outputs = {
+        self,
+        nixpkgs
+    }:
+    {
+        
     };
 }
