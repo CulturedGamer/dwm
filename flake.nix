73e88b2a1b31e7e7085ba3430fec00853693883e
CulturedGamer <dony357@outlook.com>
2023-07-31 12:41:04 -0700

    copied another flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,43 @@
-{
-    description = "Custom build of dwm";
+# {
+#     description = "Custom build of dwm";
+#
+#     outputs = { self, nixpkgs }:
+#     let
+#         system = "x86_64-linux";
+#         pkgs = import nixpkgs { inherit system; };
+#     in {
+#         packages.${system} = {
+#             dwm-custom = pkgs.dwm.overrideAttrs(_: {
+#                 version = "master";
+#                 src = ./.;
+#                 patches = [ ];
+#             });
+#             default = self.packages.${system}.dwm-custom;
+#         };
+#     };
+# }
 
-    outputs = { self, nixpkgs }:
-    let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-    in {
-        packages.${system} = {
-            dwm-custom = pkgs.dwm.overrideAttrs(_: {
-                version = "master";
-                src = ./.;
-                patches = [ ];
-            });
-            default = self.packages.${system}.dwm-custom;
-        };
-    };
+{
+  description = "Custom build of dwm";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        name = "dwm";
+        pkgs = nixpkgs.legacyPackages.${system};
+        package = pkgs.dwm.overrideAttrs (_: {
+          version = "master";
+          src = ./.;
+          patches = [ ];
+        });
+      in
+      rec {
+        apps = { ${name} = { type = "app"; program = "${defaultPackage}/bin/${name}"; }; };
+        defaultPackage = package;
+        packages.${name} = defaultPackage;
+        defaultApp = apps.${name};
+      }
+    );
 }
