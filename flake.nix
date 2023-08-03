2215ea2838f16f27eebf40bbc02bfd0992b3aed0
CulturedGamer <dony357@outlook.com>
2023-08-03 13:32:39 -0700

    commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+    description = "Description for the project";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = inputs@{ flake-parts, ... }:
+        flake-parts.lib.mkFlake { inherit inputs; } {
+            imports = [ flake-parts.flakeModules.easyOverlay ];
+            systems = [ "x86_64-linux" ];
+            perSystem = { config, pkgs, system, ... }: {
+                packages = {
+                    dwm = pkgs.dwm.overrideAttrs (oldAttrs: {
+                        version = "main";
+                        src = ./.;
+                    });
+                };
+
+                overlayAttrs = {
+                    inherit (config.packages)
+                        dwm;
+                };
+            };
+        };
+}
