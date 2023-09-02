fbe1ac8b546eaa23d804ee71b55c1f66bdc26353
veryfastman <dony357@outlook.com>
2023-09-02 12:07:52 -0700

    added nix shell & ignore executable

--- flake.nix
+++ flake.nix
@@ -6,20 +6,29 @@
     };
 
     outputs = inputs@{ flake-parts, ... }:
-        flake-parts.lib.mkFlake { inherit inputs; } {
-            imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
-            systems = [ "x86_64-linux" ];
-            perSystem = { config, pkgs, system, ... }: {
-                packages = {
-                    dwm = pkgs.dwm.overrideAttrs (oldAttrs: {
-                        version = "main";
-                        src = ./.;
-                    });
-                };
-                overlayAttrs = {
-                    inherit (config.packages)
-                        dwm;
-                };
+    flake-parts.lib.mkFlake { inherit inputs; } {
+        imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+        systems = [ "x86_64-linux" ];
+        perSystem = { config, pkgs, system, ... }: {
+            packages = {
+                dwm = pkgs.dwm.overrideAttrs (oldAttrs: {
+                    version = "main";
+                    src = ./.;
+                });
+            };
+
+            overlayAttrs = {
+                inherit (config.packages)
+                    dwm;
+            };
+
+            devShells.default = with pkgs; mkShell {
+                buildInputs = [
+                    libX11
+                    libXinerama
+                    libXft
+                ];
             };
         };
+    };
 }
